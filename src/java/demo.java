6cb69b6f9fb43f4de27336ebb194c0497f32d61b
CUI0306 <46639957+CUI0306@users.noreply.github.com>
2019-04-21 19:53:05 +0800

    create project

--- src/java/demo.java
+++ src/java/demo.java
@@ -5,5 +5,6 @@ public class demo {
         System.out.println("Hello world");
         System.out.println("dsdddd");
         System.out.println("sdddd");
+        System.out.println("sss");
     }
 }

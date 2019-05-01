7c610fee0fd626f46f0c1a27cb8d9c0b04793d80
CUI0306 <46639957+CUI0306@users.noreply.github.com>
2019-05-01 09:54:04 +0800

    ooooo

--- src/eset/demo.java
+++ src/eset/demo.java
@@ -1,4 +1,4 @@
-package java;
+package eset;
 
 public class demo {
     public static void main(String[] args) {
--- src/yset/test.java
+++ src/yset/test.java
@@ -1,4 +1,10 @@
-package testw;
+package yset;
 
 public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("dsdddd");
+        System.out.println("sdddd");
+        System.out.println("sss");
+    }
 }
